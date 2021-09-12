57933024a2646913e091900619a0a2b8990ec3ef
Rlvx <romain.4212@gmail.com>
2021-09-12 09:15:50 +0200

    Creation

---
+++ ExemplePlugin/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using Rocket.Core.Plugins;
+using Logger = Rocket.Core.Logging.Logger;
+using Rocket.Unturned;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using Color = UnityEngine.Color;
+using SDG.Unturned;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Threading;
+using System.Timers;
+using Rocket.API;
+using Rocket.Unturned.Commands;
+using Rocket.Unturned.Enumerations;
+using Rocket.Unturned.Events;
+using Steamworks;
+using SDG.Framework.Utilities;
+using UnityEngine.UI;
+using Random = System.Random;
+
+namespace ExemplePlugin
+{
+  public class Class1
+  {
+    protected void Load()
+    {
+      Logger.Log("#################################################################",ConsoleColor.Magenta);
+      Logger.Log("######################    Test Plugin    ########################",ConsoleColor.Magenta);
+      Logger.Log("#################################################################",ConsoleColor.Magenta);
+      Logger.Log("",ConsoleColor.Magenta);
+      Logger.Log("V1.0",ConsoleColor.Magenta);
+      Logger.Log("Plugin by Rom",ConsoleColor.Magenta);
+      Logger.Log("Sucessfuly Load Test Plugin",ConsoleColor.Green);
+    }
+  }
+}
---
+++ ExemplePlugin/CommandTest.cs
@@ -0,0 +1,73 @@
+using System;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using UnityEngine;
+using Rocket.API;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Rocket.Unturned.Chat;
+using SDG.Framework.Utilities;
+using UnityEngine.UI;
+using Object = System.Object;
+
+namespace TextEffect
+{
+    public class experience : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Player; }
+        }
+        public string Name
+        {
+            get { return "exp"; }
+        }
+        public string Help
+        {
+            get { return "test"; }
+        }
+        public string Syntax
+        {
+            get { return "/exp"; }
+        }
+        public List<string> Aliases
+        {
+            get { return new List<string>(); }
+        }
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>();
+            }
+        }
+        
+        public void Execute(IRocketPlayer caller, params string[] command)
+        {
+            // /exp [amount]
+            // command[0] -> amount
+            
+            UnturnedPlayer player = (UnturnedPlayer) caller;
+            
+            if (command.Length == 1)
+            {
+                if (uint.TryParse(command[0], out uint value))
+                {
+                    UnturnedChat.Say(caller,string.Format("Vous avez gagné {0} experiences",value.ToString()),Color.magenta); 
+                    player.Experience += value;
+                }
+                else
+                {
+                    UnturnedChat.Say(caller,"Syntaxe : /exp [amount:int]",Color.magenta); 
+                }
+            }
+            else
+            {
+                UnturnedChat.Say(caller,"Syntaxe : /exp [amount:int]",Color.magenta);
+            }
+                
+        }
+    }
+}
